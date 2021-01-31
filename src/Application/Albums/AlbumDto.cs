b4ebb756efc91e9e589f8f4041a0d65e9fe576b7
VladCuciureanu <vlad.c.cuciureanu@gmail.com>
2021-01-31 13:47:48 +0200

    Created DTOs and simplified DTOs for each of the main entities.

---
+++ src/Application/Albums/AlbumDto.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using AutoMapper;
+using Lyra.Application.Artists;
+using Lyra.Application.Common.Mappings;
+using Lyra.Application.Tracks;
+using Lyra.Domain.Entities;
+
+namespace Lyra.Application.Albums
+{
+    public class AlbumDto : IMapFrom<Album>
+    {
+        public AlbumDto()
+        {
+            Artists = new List<SimplifiedArtistDto>();
+            Genres = new List<string>();
+            Images = new List<Image>();
+            Tracks = new List<SimplifiedTrackDto>();
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int AlbumType { get; set; }
+
+        public DateTime? ReleaseDate { get; set; }
+        
+        public int Popularity { get; set; }
+
+        public IList<SimplifiedArtistDto> Artists { get; private set; }
+        
+        public IList<string> Genres { get; private set; }
+        
+        public IList<Image> Images { get; private set; }
+
+        public IList<SimplifiedTrackDto> Tracks { get; private set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Album, AlbumDto>()
+                .ForMember(d => d.AlbumType, opt => opt.MapFrom(s => (int) s.AlbumType));
+        }
+    }
+}
---
+++ src/Application/Artists/ArtistDto.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Lyra.Application.Albums;
+using Lyra.Application.Common.Mappings;
+using Lyra.Application.Tracks;
+using Lyra.Domain.Entities;
+
+namespace Lyra.Application.Artists
+{
+    public class ArtistDto : IMapFrom<Artist>
+    {
+        public ArtistDto()
+        {
+            Albums = new List<SimplifiedAlbumDto>();
+            Images = new List<Image>();
+            Tracks = new List<SimplifiedTrackDto>();
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Popularity { get; set; }
+
+        public IList<SimplifiedAlbumDto> Albums { get; private set; }
+
+        public IList<Image> Images { get; private set; }
+
+        public IList<SimplifiedTrackDto> Tracks { get; private set; }
+    }
+}
