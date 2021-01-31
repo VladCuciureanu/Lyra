c55d05e1f5bcd170f600f1167170cf534417cfa5
VladCuciureanu <vlad.c.cuciureanu@gmail.com>
2021-01-31 23:30:08 +0200

    Added Image entity configuration

---
+++ src/Infrastructure/Persistence/Configurations/ImageConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Lyra.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Lyra.Infrastructure.Persistence.Configurations
+{
+    public class ImageConfiguration : IEntityTypeConfiguration<Image>
+    {
+        public void Configure(EntityTypeBuilder<Image> builder)
+        {
+            builder.HasKey(e => e.Url);
+        }
+    }
+}
