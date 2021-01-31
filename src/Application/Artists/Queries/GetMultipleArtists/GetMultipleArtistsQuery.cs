5fe7a2d3a689af8fade92654464a1ffd00eab5c2
VladCuciureanu <vlad.c.cuciureanu@gmail.com>
2021-02-01 00:29:20 +0200

    Implement GET /api/v1/artist endpoint

---
+++ src/Application/Artists/Queries/GetMultipleArtists/GetMultipleArtistsQuery.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Lyra.Application.Common.Interfaces;
+using Lyra.Application.Common.Mappings;
+using Lyra.Application.Common.Models;
+using Lyra.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lyra.Application.Artists.Queries.GetMultipleArtists
+{
+    public class GetMultipleArtistsQuery : IRequest<List<ArtistDto>>
+    {
+        [Required] public List<int> Ids { get; set; }
+    }
+
+    public class GetMultipleArtistsQueryHandler : IRequestHandler<GetMultipleArtistsQuery, List<ArtistDto>>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetMultipleArtistsQueryHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<List<ArtistDto>> Handle(GetMultipleArtistsQuery request, CancellationToken cancellationToken)
+        {
+            var result = new List<ArtistDto>();
+            
+            foreach (var requestId in request.Ids)
+            {
+                var artist = _context.Artists
+                    .Include(e => e.Images)
+                    .Include(e => e.Genres)
+                    .SingleOrDefault(e => e.Id.Equals(requestId));
+
+                result.Add(_mapper.Map<ArtistDto>(artist));
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/Application/Artists/Queries/GetMultipleArtists/GetMultipleArtistsQueryValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Lyra.Application.Artists.Queries.GetMultipleArtists
+{
+    public class GetMultipleArtistsQueryValidator : AbstractValidator<GetMultipleArtistsQuery>
+    {
+        public GetMultipleArtistsQueryValidator()
+        {
+            RuleFor(e => e.Ids).Must(q => q.Count <= 50).WithMessage("Upper limit of fetches in one query is of 50 artists.");
+        }
+    }
+}
