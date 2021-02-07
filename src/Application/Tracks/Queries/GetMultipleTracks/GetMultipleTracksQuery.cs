a4b3f0b6fc62b4b8e9d16d2d3eb5d8293edfd32f
VladCuciureanu <vlad.c.cuciureanu@gmail.com>
2021-02-07 14:33:18 +0200

    Implemented GET /api/v1/track

---
+++ src/Application/Tracks/Queries/GetMultipleTracks/GetMultipleTracksQuery.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using Lyra.Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lyra.Application.Tracks.Queries.GetMultipleTracks
+{
+    public class GetMultipleTracksQuery : IRequest<List<TrackDto>>
+    {
+        [Required] public List<int> Ids { get; set; }
+    }
+
+    public class GetMultipleTracksQueryHandler : IRequestHandler<GetMultipleTracksQuery, List<TrackDto>>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetMultipleTracksQueryHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<List<TrackDto>> Handle(GetMultipleTracksQuery request, CancellationToken cancellationToken)
+        {
+            var result = new List<TrackDto>();
+
+            foreach (var requestId in request.Ids)
+            {
+                var track = await _context.Tracks
+                    .Include(e => e.Album).ThenInclude(t => t.Images)
+                    .Include(e => e.Album).ThenInclude(t => t.Artists)
+                    .Include(e => e.Artists).ThenInclude(a => a.Genres)
+                    .Include(e => e.Artists).ThenInclude(a => a.Images)
+                    .SingleOrDefaultAsync(e => e.Id.Equals(requestId));
+                
+                result.Add(_mapper.Map<TrackDto>(track));
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/Application/Tracks/Queries/GetMultipleTracks/GetMultipleTracksQueryValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Lyra.Application.Tracks.Queries.GetMultipleTracks
+{
+    public class GetMultipleTracksQueryValidator : AbstractValidator<GetMultipleTracksQuery>
+    {
+        public GetMultipleTracksQueryValidator()
+        {
+            RuleFor(e => e.Ids).Must(q => q.Count <= 50).WithMessage("Upper limit of fetches in one query is of 50 artists.");
+        }
+    }
+}
