40701ff224695a832e2c41f73f3f31067306e3d5
VladCuciureanu <vlad.c.cuciureanu@gmail.com>
2021-02-07 23:40:50 +0200

    Implemented POST /api/v1/artist

---
+++ src/Application/Artists/Commands/CreateArtist/CreateArtistCommand.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Threading;
+using System.Threading.Tasks;
+using Lyra.Application.Common.Interfaces;
+using Lyra.Domain.Entities;
+using Lyra.Domain.Events;
+using MediatR;
+
+namespace Lyra.Application.Artists.Commands.CreateArtist
+{
+    public class CreateArtistCommand : IRequest<int>
+    {
+        [Required] public string Name { get; set; }
+    }
+
+    public class CreateArtistCommandHandler : IRequestHandler<CreateArtistCommand, int>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public CreateArtistCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Handle(CreateArtistCommand request, CancellationToken cancellationToken)
+        {
+            var entity = new Artist
+            {
+                Name = request.Name
+            };
+
+            entity.DomainEvents.Add(new ArtistCreatedEvent(entity));
+
+            await _context.Artists.AddAsync(entity, cancellationToken);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return entity.Id;
+        }
+    }
+}
