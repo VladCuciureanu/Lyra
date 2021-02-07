4fe3ebb08b0f63d69d9f73e35f82d008bcff09a7
VladCuciureanu <vlad.c.cuciureanu@gmail.com>
2021-02-07 14:23:28 +0200

    Added GetTrackQueryValidator.cs

---
+++ src/Application/Tracks/Queries/GetTrack/GetTrackQueryValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Lyra.Application.Tracks.Queries.GetTrack
+{
+    public class GetTrackQueryValidator : AbstractValidator<GetTrackQuery>
+    {
+        public GetTrackQueryValidator()
+        {
+            RuleFor(q => q.Id).GreaterThanOrEqualTo(1).WithMessage("Entity Ids are always greater or equal to 1.");
+        }
+    }
+}
